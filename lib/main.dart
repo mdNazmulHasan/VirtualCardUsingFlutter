0c4ca6b75b605700d3b361f46713aa2348bb28ea
mdNazmulHasan <najmul.hasan10@gmail.com>
2020-01-09 11:46:15 +0600

    container added in safe area

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MiCard());
+}
+
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.all(20),
+            color: Colors.white,
+            child: Text("hello"),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
